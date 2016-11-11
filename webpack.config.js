934eb9b47ede26ca342f58a19c0f66ff3bea69ac
yaroslav <yaroslav.hryhoruk@gmail.com>
2016-11-11 18:25:45 +0200

    Creates  structure

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    'babel-polyfill',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    preLoaders: [ //добавили ESlint в preloaders
+      {
+        test: /\.js$/,
+        loaders: ['eslint'],
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+      }
+    ],
+    loaders: [
+      {
+        loaders: ['react-hot-loader/webpack', 'babel-loader'],
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        test: /\.js$/,
+        plugins: ['transform-runtime'],
+      }
+    ]
+  }
+}
