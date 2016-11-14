389a0764730af3526ff959162eb49d62f3d4b1ed
yaroslav <yaroslav.hryhoruk@gmail.com>
2016-11-14 14:32:41 +0200

    Create aplication without VK

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import User from '../components/User'
+import Page from '../components/Page'
+import * as pageActions from '../actions/PageActions'
 
-export default class App extends Component {
+class App extends Component {
   render() {
-    return <div>Привет из App</div>
+    const { user, page } = this.props
+    const { getPhotos } = this.props.pageActions
+
+    return <div>
+      <User name={user.name} />
+      <Page photos={page.photos} year={page.year} getPhotos={getPhotos} fetching={page.fetching}/>
+    </div>
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+    page: state.page
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    pageActions: bindActionCreators(pageActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
